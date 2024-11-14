8f4047ffa210c94bad4a3fca6c0e4b220e74435a
mwavafp <mwavaferdiansyahputra@gmail.com>
2024-11-15 01:53:47 +0700

    menyelesaikan navigasi

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,7 @@
-<!--Tempat atur link -->
-<a {{$attributes}}
-  class={{request()->is(`$attributes`)?' bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}} aria-current="page">{{$slot}}</a>
+<!-- Komponen x-nav-link -->
+@props(['href'])
+<a {{ $attributes}} 
+   class="{{ request()->is($href) ? 'bg-red-500 text-white' : 'text-orange hover:text-orangehrv hover:bg-red-200 rounded-md' }} px-4 py-2 rounded-md" 
+   aria-current="page">
+   {{ $slot }}
+</a>
