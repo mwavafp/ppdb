c93d1e35bbb1a45d37b557fc925e80a42f2b9d80
unknown <fatwabawahsi26@gmail.com>
2024-12-27 04:03:16 +0700

    Update SeleksiAdminController, paginate (done)

--- routes/pageAuth.php
+++ routes/pageAuth.php
@@ -4,16 +4,14 @@ use App\Http\Controllers\Admin\Auth\AdminDashboardController;
 use App\Http\Controllers\Admin\Auth\KelasController;
 use App\Http\Controllers\Admin\Auth\TagihanAdmin;
 use App\Http\Controllers\Admin\Auth\SiswaController;
+use App\Http\Controllers\Admin\Auth\SeleksiAdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard-admin', [AdminDashboardController::class, 'showUser']);
 Route::get('/pembagiankelas', [KelasController::class, 'index']);
 
 Route::get('/siswa', [SiswaController::class, 'index'])->name('index');
-Route::get('/seleksiSiswa', function () {
-    return view('admin.page.seleksi', ['title' => 'Seleksi Siswa Page']);
-});
-
+Route::get('/seleksi-siswa', [SeleksiAdminController::class, 'showData'])->name('seleksi-admin');
 Route::get('/tagihan-admin', [TagihanAdmin::class, 'showData'])->name('tagihan-admin');
 Route::get('/edit-tagihan/{id}', [TagihanAdmin::class, 'editData'])->name('edit-tagihan');
 Route::post('update-tagihan/{id}', [TagihanAdmin::class, 'updateData'])->name('update-tagihan');
