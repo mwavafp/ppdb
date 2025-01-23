6e9099ba733b1e5bf30fd6d41b757b9796dc6065
elsanu <en71194@gmail.com>
2025-01-23 22:46:06 +0700

    update...

--- resources/views/frontPage/pondok.blade.php
+++ resources/views/frontPage/pondok.blade.php
@@ -1,14 +1,48 @@
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    <div class="bg-gray-100 ">
+    <div class="bg-gray-100 my-4  ">
         <main class="container mx-auto mt-6">
             <div class="bg-primary text-white text-center py-4 rounded-md">
                 <h1 class="text-xl font-bold">
                     INFORMASI PONDOK
                 </h1>
             </div>
+            <section class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+    <div class="bg-white p-4 rounded-md shadow-md">
+      <img src="/images/compo_pondok.jpg" alt="" class="w-50 h-50 mx-auto mb-12">
+    </div>
+    <div class="bg-white p-4 rounded-md shadow-md">
+     <h2 class="text-lg font-bold">
+      Company Profile
+     </h2>
+     <p>
+      Lakukan pembayaran sesuai nominal dan nomer rekening yang tertera
+     </p>
+    </div>
+   </section>
+   <section class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+    <div class="bg-white p-4 rounded-md shadow-md">
+      <center>
+     <h2 class="text-primary text-lg font-bold mb-4">
+      VISI
+     </h2>
+     </center>
+     <p class="mt-4">
+      Berakhlak Mulia Dan Beribadah Sempurna Adalah karakter Siswa Santri Nurul Huda
+     </p>
+    </div>
+    <div class="bg-white p-4 rounded-md shadow-md">
+      <center>
+     <h2 class="text-primary text-lg font-bold mb-4">
+      MISI
+     </h2>
+</center>
+     <p class="mt-4">
+      STEP 2 - ISI FORMULIR PENDAFTARAN
+     </p>
+    </div>
+   </section>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 m-6">
 
                 <div class="bg-white p-6 rounded-md shadow-md">
@@ -115,28 +149,35 @@
                                 <i class="fas fa-check text-primary">
                                 </i>
                                 <span class="ml-2">
-                                    Akta Kelahiran
+                                Fotocopy Ijazah 3 Lembar
                                 </span>
                             </li>
                             <li class="flex items-center">
                                 <i class="fas fa-check text-primary">
                                 </i>
                                 <span class="ml-2">
-                                    Kartu Keluarga
+                                Fotocopy SKHUN 3 Lembar
                                 </span>
                             </li>
                             <li class="flex items-center">
                                 <i class="fas fa-check text-primary">
                                 </i>
                                 <span class="ml-2">
-                                    Data Diri Anak
+                                Fotocopy Akte Kelahiran 3 Lembar
                                 </span>
                             </li>
                             <li class="flex items-center">
                                 <i class="fas fa-check text-primary">
                                 </i>
                                 <span class="ml-2">
-                                    Data Diri Orang Tua
+                                Fotocopy KK 3 Lembar
+                                </span>
+                            </li>
+                            <li class="flex items-center">
+                                <i class="fas fa-check text-primary">
+                                </i>
+                                <span class="ml-2">
+                                Foto 3x4 3 Lembar
                                 </span>
                             </li>
                         </ul>
@@ -144,116 +185,9 @@
                 </div>
 
             </div>
-           </li>
-           <li class="flex items-start">
-            <i class="fas fa-check text-green-500 mt-1">
-            </i>
-            <div class="ml-2">
-             <strong>
-              STEP 3 - PEMBAYARAN
-             </strong>
-             <p>
-              Lakukan pembayaran sesuai nominal dan nomer rekening yang tertera
-             </p>
-            </div>
-           </li>
-           <li class="flex items-start">
-            <i class="fas fa-check text-green-500 mt-1">
-            </i>
-            <div class="ml-2">
-             <strong>
-              STEP 4 - ISI BIODATA
-             </strong>
-             <p>
-              Lengkapi biodata calon murid dan orang tua guna pendataan.
-             </p>
-            </div>
-           </li>
-           <li class="flex items-start">
-            <i class="fas fa-check text-green-500 mt-1">
-            </i>
-            <div class="ml-2">
-             <strong>
-              STEP 5 - CETAK KARTU
-             </strong>
-             <p>
-              Lakukan cetak kartu dan simpan sebagai bukti pada saat registrasi ulang.
-             </p>
-            </div>
-           </li>
-          </ul>
-         </div>
-         <div class='flex flex-col'>
-         <div class="bg-white p-6 rounded-md shadow-md mb-4 tabel-1">
-          <h2 class="text-orange-600 text-lg font-bold mb-4">
-           JADWAL PENDAFTARAN
-          </h2>
-          <ul class="space-y-2 mb-4">
-           <li>
-            Gelombang 1 :
-            <br/>
-            2 Desember 2024 - 28 Februari 2025
-           </li>
-           <li>
-            Gelombang 2 :
-            <br/>
-            1 Maret - 31 Mei 2025
-           </li>
-           <li>
-            Gelombang 3 :
-            <br/>
-            1 Juni - 14 Juli 2025
-           </li>
-          </ul>
-          <a href="/form?unit_pendidikan=SMP" class="bg-orange-600 text-white py-2 px-4 rounded-md">
-         DAFTAR KLIK DISINI
-        </a>
-         </div>
-         <div class="bg-white p-6 rounded-md shadow-md  tabel-2">
-          <h2 class="text-orange-600 text-lg font-bold mt-6 mb-4">
-           BERKAS YANG HARUS DISIAPKAN
-          </h2>
-          <ul class="space-y-2">
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-             Fotocopy Ijazah 3 Lembar
-            </span>
-           </li>
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-            Fotocopy SKHUN 3 Lembar
-            </span>
-           </li>
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-            Fotocopy Akte Kelahiran 3 Lembar
-            </span>
-           </li>
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-            Fotocopy KK 3 Lembar
-            </span>
-           </li>
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-            Foto 3x4 3 Lembar
-            </span>
-           </li>
-          </ul>
-         </div>
-         </div>
-         
-        </div>
+      
+           
+           
         
        </main>
 </div>
--- resources/views/frontPage/sma.blade.php
+++ resources/views/frontPage/sma.blade.php
@@ -1,172 +1,209 @@
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
 <x-layout>
-    <x-slot:title>{{$title}}</x-slot:title>
+  <x-slot:title>{{$title}}</x-slot:title>
 
-<div class="bg-gray-100">
-    <main class="container mx-auto mt-6">
-        <div class="bg-red-600 text-white text-center py-4 rounded-md">
-         <h1 class="text-xl font-bold">
-          INFORMASI SMA
-         </h1>
-        </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 m-6">
-         
-         <div class="bg-white p-6 rounded-md shadow-md">
-          <h2 class="text-orange-600 text-lg font-bold mb-4">
-           ALUR PENDAFTARAN
-          </h2>
-          <ul class="space-y-4">
-           <li class="flex items-start">
-            <i class="fas fa-check text-green-500 mt-1">
-            </i>
-            <div class="ml-2">
-             <strong>
-              STEP 1 - DAFTAR
-             </strong>
-             <p>
-              Selesai mendaftar, pendaftar akan menerima notifikasi whatsapp untuk melanjutkan proses pembayaran
-             </p>
-            </div>
-           </li>
-           <li class="flex items-start">
-            <i class="fas fa-check text-green-500 mt-1">
-            </i>
-            <div class="ml-2">
-             <strong>
-              STEP 2 - ISI FORMULIR PENDAFTARAN
-             </strong>
-             <p>
-              Isi dan lengkapi isi formulir pendaftaran dan pilih tujuan daftar (Daycare, Playgroup, TK).
-             </p>
-            </div>
-           </li>
-           <li class="flex items-start">
-            <i class="fas fa-check text-green-500 mt-1">
-            </i>
-            <div class="ml-2">
-             <strong>
-              STEP 3 - PEMBAYARAN
-             </strong>
-             <p>
-              Lakukan pembayaran sesuai nominal dan nomer rekening yang tertera
-             </p>
-            </div>
-           </li>
-           <li class="flex items-start">
-            <i class="fas fa-check text-green-500 mt-1">
-            </i>
-            <div class="ml-2">
-             <strong>
-              STEP 4 - ISI BIODATA
-             </strong>
-             <p>
-              Lengkapi biodata calon murid dan orang tua guna pendataan.
-             </p>
-            </div>
-           </li>
-           <li class="flex items-start">
-            <i class="fas fa-check text-green-500 mt-1">
-            </i>
-            <div class="ml-2">
-             <strong>
-              STEP 5 - CETAK KARTU
-             </strong>
-             <p>
-              Lakukan cetak kartu dan simpan sebagai bukti pada saat registrasi ulang.
-             </p>
-            </div>
-           </li>
-          </ul>
-         </div>
-         <div class='flex flex-col'>
-         <div class="bg-white p-6 rounded-md shadow-md mb-4 tabel-1">
-          <h2 class="text-orange-600 text-lg font-bold mb-4">
-           JADWAL PENDAFTARAN
-          </h2>
-          <ul class="space-y-2 mb-4">
-           <li>
-            Gelombang 1 :
-            <br/>
-            2 Desember 2024 - 28 Februari 2025
-           </li>
-           <li>
-            Gelombang 2 :
-            <br/>
-            1 Maret - 31 Mei 2025
-           </li>
-           <li>
-            Gelombang 3 :
-            <br/>
-            1 Juni - 14 Juli 2025
-           </li>
-          </ul>
-          <a href="/form?unit_pendidikan=SMP" class="bg-orange-600 text-white py-2 px-4 rounded-md">
+<div class="bg-gray-100 my-4  ">
+  <main class="container mx-auto mt-6">
+      <div class="bg-primary text-white text-center py-4 rounded-md">
+       <h1 class="text-xl font-bold">
+        INFORMASI SMP
+       </h1>
+      </div>
+      <section class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+    <div class="bg-white p-4 rounded-md shadow-md">
+      <img src="/images/compro_sma.jpg" alt="" class="w-50 h-50 mx-auto mb-12">
+    </div>
+    <div class="bg-white p-4 rounded-md shadow-md">
+     <h2 class="text-lg font-bold">
+      Company Profile
+     </h2>
+     <p>
+      Lakukan pembayaran sesuai nominal dan nomer rekening yang tertera
+     </p>
+    </div>
+   </section>
+   <section class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+    <div class="bg-white p-4 rounded-md shadow-md">
+      <center>
+     <h2 class="text-primary text-lg font-bold mb-4">
+      VISI
+     </h2>
+     </center>
+     <p class="mt-4">
+     "MENCETAK GENERASI MASA DEPAN YANG BERTAQWA, BERILMU DAN BERAKHLAQUL KARIMAH".
+     </p>
+    </div>
+    <div class="bg-white p-4 rounded-md shadow-md">
+      <center>
+     <h2 class="text-primary text-lg font-bold mb-4">
+      MISI
+     </h2>
+</center>
+     <p class="mt-4">
+     <ol class="list-decimal list-inside space-y-2">
+     <li class="text-gray-700"> Menegakkan dan menjunjung tinggi nilai-nilai ajaran islan yang bermaktub dalam Al-Qur'an dan Al-Hadits serta keteladanan Ulama Sholih.</li> <br>
+     <li class="text-gray-700"> Menjunjung tinggi nama baik Almamater Yayasan Pondok Pesantren Nurul Huda.</li> <br>
+     <li class="text-gray-700"> Menanamkan akhlakul karimah dalam bersikap, berbuat, berkata, dan berbusana di lingkungan Pesantren Nahdlatul Ulama.</li> <br>
+     <li class="text-gray-700"> Memberikan dasar arah dan pedoman berperilaku selama studi di Yayasan Pondok Pesantren Nurul Huda.</li> 
+</ol>
+     </p>
+    </div>
+   </section>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6 m-6">
+       
+       <div class="bg-white p-6 rounded-md shadow-md">
+        <h2 class="text-primary text-lg font-bold mb-4">
+         ALUR PENDAFTARAN
+        </h2>
+        <ul class="space-y-4">
+         <li class="flex items-start">
+          <i class="fas fa-check text-green-500 mt-1">
+          </i>
+          <div class="ml-2">
+           <strong>
+            STEP 1 - DAFTAR
+           </strong>
+           <p>
+            Selesai mendaftar, pendaftar akan menerima notifikasi whatsapp untuk melanjutkan proses pembayaran
+           </p>
+          </div>
+         </li>
+         <li class="flex items-start">
+          <i class="fas fa-check text-green-500 mt-1">
+          </i>
+          <div class="ml-2">
+           <strong>
+            STEP 2 - ISI FORMULIR PENDAFTARAN
+           </strong>
+           <p>
+            Isi dan lengkapi isi formulir pendaftaran dan pilih tujuan daftar (Daycare, Playgroup, TK).
+           </p>
+          </div>
+         </li>
+         <li class="flex items-start">
+          <i class="fas fa-check text-green-500 mt-1">
+          </i>
+          <div class="ml-2">
+           <strong>
+            STEP 3 - PEMBAYARAN
+           </strong>
+           <p>
+            Lakukan pembayaran sesuai nominal dan nomer rekening yang tertera
+           </p>
+          </div>
+         </li>
+         <li class="flex items-start">
+          <i class="fas fa-check text-green-500 mt-1">
+          </i>
+          <div class="ml-2">
+           <strong>
+            STEP 4 - ISI BIODATA
+           </strong>
+           <p>
+            Lengkapi biodata calon murid dan orang tua guna pendataan.
+           </p>
+          </div>
+         </li>
+         <li class="flex items-start">
+          <i class="fas fa-check text-green-500 mt-1">
+          </i>
+          <div class="ml-2">
+           <strong>
+            STEP 5 - CETAK KARTU
+           </strong>
+           <p>
+            Lakukan cetak kartu dan simpan sebagai bukti pada saat registrasi ulang.
+           </p>
+          </div>
+         </li>
+        </ul>
+       </div>
+       <div class='flex flex-col'>
+       <div class="bg-white p-6 rounded-md shadow-md mb-4 tabel-1">
+        <h2 class="text-primary text-lg font-bold mb-4">
+         JADWAL PENDAFTARAN
+        </h2>
+        <ul class="space-y-2 mb-4">
+         <li>
+          Gelombang 1 :
+          <br/>
+          2 Desember 2024 - 28 Februari 2025
+         </li>
+         <li>
+          Gelombang 2 :
+          <br/>
+          1 Maret - 31 Mei 2025
+         </li>
+         <li>
+          Gelombang 3 :
+          <br/>
+          1 Juni - 14 Juli 2025
+         </li>
+        </ul>
+        <a href="/form?unit_pendidikan=SMP" class="bg-primary text-white py-2 px-4 rounded-md">
          DAFTAR KLIK DISINI
         </a>
-         </div>
-         <div class="bg-white p-6 rounded-md shadow-md  tabel-2">
-          <h2 class="text-orange-600 text-lg font-bold mt-6 mb-4">
-           BERKAS YANG HARUS DISIAPKAN
-          </h2>
-          <ul class="space-y-2">
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-             Fotocopy Ijazah di Legalisir 3 Lembar
-            </span>
-           </li>
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-            Fotocopy SKHNU di Legalisir 3 Lembar
-            </span>
-           </li>
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-            Fotocopy AKte Kelahiran 3 Lembar
-            </span>
-           </li>
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-            Fotocopy KK di Legalisir 3 Lembar
-            </span>
-           </li>
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-            Foto Warna 4x6 2 Lembar
-            </span>
-           </li>
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-            Foto Warna 3x4 2 Lembar
-            </span>
-           </li>
-           <li class="flex items-center">
-            <i class="fas fa-check text-green-500">
-            </i>
-            <span class="ml-2">
-            Foto Hitam Putih 3x4 2 Lembar
-            </span>
-           </li>
-          </ul>
-         </div>
-         </div>
-         
-        </div>
-        
-       </main>
+       </div>
+       <div class="bg-white p-6 rounded-md shadow-md  tabel-2">
+        <h2 class="text-primary text-lg font-bold mt-6 mb-4">
+         BERKAS YANG HARUS DISIAPKAN
+        </h2>
+        <ul class="space-y-2">
+         <li class="flex items-center">
+          <i class="fas fa-check text-primary">
+          </i>
+          <span class="ml-2">
+          Fotocopy Ijazah di Legalisir 3 Lembar
+          </span>
+         </li>
+         <li class="flex items-center">
+          <i class="fas fa-check text-primary">
+          </i>
+          <span class="ml-2">
+          Fotocopy SKHUN di Legalisir 3 Lembar
+          </span>
+         </li>
+         <li class="flex items-center">
+          <i class="fas fa-check text-primary">
+          </i>
+          <span class="ml-2">
+          Fotocopy KK di Legalisir 3 Lembar
+          </span>
+         </li>
+         <li class="flex items-center">
+          <i class="fas fa-check text-primary">
+          </i>
+          <span class="ml-2">
+          Fotocopy Akte Kelahiran 3 Lembar
+          </span>
+         </li>
+         <li class="flex items-center">
+          <i class="fas fa-check text-primary">
+          </i>
+          <span class="ml-2">
+          Foto Warna 4x6 2 Lembar
+          </span>
+         </li>
+         <li class="flex items-center">
+          <i class="fas fa-check text-primary">
+          </i>
+          <span class="ml-2">
+          Foto Warna 3x4 2 Lembar
+          </span>
+         </li>
+         <li class="flex items-center">
+          <i class="fas fa-check text-primary">
+          </i>
+          <span class="ml-2">
+          Foto Hitam Putih 3x4 2 Lembar
+          </span>
+         </li>
+        </ul>
+       </div>
+       </div>
+       
+      </div>
+      
+     </main>
 </div>
- 
- 
 
 </x-layout>
