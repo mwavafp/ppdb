ff81e100cbb1db55f0eb3abd58eddfaa5fa5c344
amrthyb <3130022011@student.unusa.ac.id>
2025-05-13 14:14:00 +0700

    visibility login

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -15,14 +15,37 @@
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        {{-- <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
                 autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div> --}}
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <div class="relative">
+                <x-text-input id="password" class="block mt-1 w-full pr-10" type="password" name="password" required
+                    autocomplete="current-password" />
+
+                <span class="absolute top-1/2 right-3 -translate-y-1/2 transform cursor-pointer"
+                    onclick="togglePassword()">
+                    <svg id="eyeIcon" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5
+                    c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7
+                    -4.477 0-8.268-2.943-9.542-7z" />
+                    </svg>
+                </span>
+            </div>
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
+
 
         <!-- Remember Me -->
         <div class="block mt-4">
@@ -46,4 +69,34 @@
             </x-primary-button>
         </div>
     </form>
+
+    <script>
+        function togglePassword() {
+            const input = document.getElementById('password');
+            const icon = document.getElementById('eyeIcon');
+
+            if (input.type === 'password') {
+                input.type = 'text';
+                icon.innerHTML = `
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M13.875 18.825A10.05 10.05 0 0112 19
+                    c-4.477 0-8.268-2.943-9.542-7a9.953 9.953 0 012.182-3.362
+                    m3.687-2.58A9.956 9.956 0 0112 5c4.477 0 8.268 2.943
+                    9.542 7a9.956 9.956 0 01-1.507 2.888M15 12a3 3 0 11-6 0
+                    3 3 0 016 0zm6 6L6 6" />
+            `;
+            } else {
+                input.type = 'password';
+                icon.innerHTML = `
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5
+                    c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7
+                    -4.477 0-8.268-2.943-9.542-7z" />
+            `;
+            }
+        }
+    </script>
+
 </x-guest-layout>
